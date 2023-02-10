ccbac6f7a6cdc8ad14d041ebd6c12582ddac78b7
murilo9 <murilohenriquematias@gmail.com>
2023-02-10 18:29:07 -0300

    fixed default private child route

--- src/routes/private.tsx
+++ src/routes/private.tsx
@@ -47,7 +47,7 @@ export default function PrivateRoutes() {
                 element={<PeoplePage />}
               />
               <Route
-                path="*"
+                path="/"
                 element={
                   <Navigate
                     to="/boards"
